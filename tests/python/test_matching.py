71ee1298091278299d9e3a4d0fe6cd7b8bcf3615
Jean-Christophe Ruel <ruelj2@gmail.com>
2024-07-27 13:38:51 -0400

    test: fix matching tests

--- tests/python/test_matching.py
+++ tests/python/test_matching.py
@@ -9,88 +9,67 @@ def make_rotation(line_angle):
 
 def create_lines(line_number, length):
     line_array = np.zeros((4, line_number))
-    for i in range(line_number):
-        line_angle = float(i) / float(line_number) * np.pi - np.pi / 2
+    for i, line_angle in enumerate(np.logspace(np.log10(2 * np.pi), np.log10(4 * np.pi), line_number)):
         rotation_matrix = make_rotation(line_angle)
-        endpoint = np.dot(rotation_matrix, np.array([length, 0]))
+        endpoint = np.matmul(rotation_matrix, np.array([length, 0]))
         line_array[:, i] = np.array([0, 0, endpoint[0], endpoint[1]])
     return line_array
 
 def all_close(a, b, atol=1e-5):
     return np.allclose(a, b, atol=atol)
 
-def test_default_match():
-    max_tmpl_lines, max_scene_lines = 4, 4
-    depth = 4
-    scene_ratio = 1.0
+def apply_transform(lines, transform):
+    return (np.matmul(transform[:2, :2], lines.reshape(2, -1)) + transform[:2, 2:3]).reshape(4, -1)
+
+def run_test(scene_ratio):
+    max_tmpl_lines, max_scene_lines = 4, 10
+    depth = 30
     scene_padding = 2.2
-    coeff = 50.0
+    coeff = 5.0
     search_strategy = DefaultSearch(max_tmpl_lines, max_scene_lines)
     optimizer_strategy = DefaultOptimize()
     matcher = DefaultMatch(depth, coeff, scene_ratio, scene_padding)
-    tmpl = create_lines(4, 10)
+    number_of_lines = 10
+    line_length = 100
+    tmpl = create_lines(number_of_lines, line_length)
 
-    rotation = np.array([[-1, 0], [0, -1]])
-    templates = [np.dot(rotation, tmpl.reshape(2, -1)).reshape(4, -1)]
+    scene_transform = np.array([[-1, 0, line_length], [0, -1, line_length]])
+    scene = apply_transform(tmpl, scene_transform)
+    matches = search(matcher, search_strategy, optimizer_strategy, [tmpl], scene)
+    best_match_transform = matches[0].transform
+    best_match_rotation = best_match_transform[:2, :2]
+    best_match_translation = best_match_transform[:2, 2]
 
-    scene = tmpl
-    matches = search(matcher, search_strategy, optimizer_strategy, templates, scene)
-    result_rotation = matches[0].transform[0:2, 0:2]
-    result_translation = matches[0].transform[0:2, 2]
+    assert len(matches) == min(max_tmpl_lines, number_of_lines) * min(number_of_lines, max_scene_lines) * 2
+    assert all_close(scene_transform[:2, :2], best_match_rotation)
+    assert all_close(scene_transform[:2, 2], best_match_translation, 1e0 * 1 / scene_ratio)
 
-    print("Matches:\n", matches)
+    scene_transform = np.array([[1, 0, 0], [0, 1, 0]])
+    scene = apply_transform(tmpl, scene_transform)
+    matches = search(matcher, search_strategy, optimizer_strategy, [tmpl], scene)
+    best_match_rotation = matches[0].transform[:2, :2]
+    best_match_translation = matches[0].transform[:2, 2]
 
     assert len(matches) == max_tmpl_lines * max_scene_lines * 2
-    assert all_close(result_rotation, rotation)
-    assert all_close(result_translation, np.array([0, 0]))
+    assert all_close(scene_transform[:2, :2], best_match_rotation)
+    assert all_close(scene_transform[:2, 2], best_match_translation, 1e0 * 1 / scene_ratio)
 
-    translation = np.array([[5], [-5]])
-    templates = [(tmpl.reshape(2,-1) + translation).reshape(4,-1)]
-    matches = search(matcher, search_strategy, optimizer_strategy, templates, scene)
-    result_rotation = matches[0].transform[0:2, 0:2]
-    result_translation = matches[0].transform[0:2, 2]
-
-    assert len(matches) == max_tmpl_lines * max_scene_lines * 2
-    assert all_close(result_rotation, np.eye(2))
-    assert all_close(result_translation, -translation)
-    assert matches[0].score == 0.0
-
-    templates = [tmpl]
     scene = np.zeros((4, 0))
-    matches = search(matcher, search_strategy, optimizer_strategy, templates, scene)
+    matches = search(matcher, search_strategy, optimizer_strategy, [tmpl], scene)
     assert len(matches) == 0
 
     templates = []
     scene = tmpl
-    matches = search(matcher, search_strategy, optimizer_strategy, templates, scene)
+    matches = search(matcher, search_strategy, optimizer_strategy, [], scene)
     assert len(matches) == 0
 
     templates = [np.zeros((4, 0))]
     matches = search(matcher, search_strategy, optimizer_strategy, templates, scene)
     assert len(matches) == 0
 
-def test_scale_down_scene():
-    max_tmpl_lines, max_scene_lines = 4, 4
-    depth = 4
-    scene_ratio = 0.3
-    scene_padding = 2.2
-    coeff = 5.0
-    matcher = DefaultMatch(depth, coeff, scene_ratio, scene_padding)
-    search_strategy = DefaultSearch(max_tmpl_lines, max_scene_lines)
-    optimizer_strategy = DefaultOptimize()
-    tmpl = create_lines(4, 10)
-
-    rotation = np.array([[-1, 0], [0, -1]])
-    templates = [np.dot(rotation, tmpl.reshape(2, -1)).reshape(4, -1).astype(np.float64)]
-    scene = tmpl
-    matches = search(matcher, search_strategy, optimizer_strategy, templates, scene.astype(np.float64))
-    result_rotation = matches[0].transform[0:2, 0:2]
-    result_translation = matches[0].transform[0:2, 2]
-
-    assert len(matches) == max_tmpl_lines * max_scene_lines * 2
-    assert all_close(result_rotation, rotation)
-    assert all_close(result_translation, np.array([0, 0]))
-    assert matches[0].score < 2.4
+@pytest.mark.parametrize("scene_ratio", [1.0, 0.3])
+def test_matching(scene_ratio):
+    run_test(scene_ratio)
 
 if __name__ == "__main__":
     pytest.main()
