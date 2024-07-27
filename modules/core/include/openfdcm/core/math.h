161da7e68f13d2d410e2755e5e03c28028c01d37
Jean-Christophe Ruel <ruelj2@gmail.com>
2024-07-27 16:55:14 -0400

    fix: define non-standart C++ constants for MAC OS build

--- modules/core/include/openfdcm/core/math.h
+++ modules/core/include/openfdcm/core/math.h
@@ -32,6 +32,13 @@ SOFTWARE.
 #include <type_traits>
 #include <Eigen/Dense>
 
+#define 	M_PI   3.14159265358979323846 /* pi */
+#define 	M_PIf   3.14159265358979323846f /* pi */
+#define 	M_PI_2   1.57079632679489661923 /* pi/2 */
+#define 	M_PI_2f   1.57079632679489661923f /* pi/2 */
+#define 	M_PI_4   0.78539816339744830962 /* pi/4 */
+#define 	M_PI_4f   0.78539816339744830962f /* pi/4 */
+
 namespace openfdcm::core
 {
     template<typename T> using RawImage = Eigen::Array<T, -1, -1, Eigen::ColMajor>;
