fb895bd246530e28935a813482b2c0da98c3a4d7
Jean-Christophe Ruel <ruelj2@gmail.com>
2024-08-15 11:56:37 -0400

    fix: conditionnal define M_PI

--- modules/core/include/openfdcm/core/math.h
+++ modules/core/include/openfdcm/core/math.h
@@ -32,12 +32,13 @@ SOFTWARE.
 #include <type_traits>
 #include <Eigen/Dense>
 
+#ifndef M_PI
 #define 	M_PI   3.14159265358979323846 /* pi */
-#define 	M_PIf   3.14159265358979323846f /* pi */
 #define 	M_PI_2   1.57079632679489661923 /* pi/2 */
 #define 	M_PI_2f   1.57079632679489661923f /* pi/2 */
 #define 	M_PI_4   0.78539816339744830962 /* pi/4 */
 #define 	M_PI_4f   0.78539816339744830962f /* pi/4 */
+#endif
 
 namespace openfdcm::core
 {
