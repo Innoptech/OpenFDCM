b322df5133148980cc1268e2769772a0facef451
Jean-Christophe Ruel <ruelj2@gmail.com>
2024-08-16 10:36:48 -0400

    feat: simplify serialization with packio

--- modules/core/include/openfdcm/core/serialization.h
+++ modules/core/include/openfdcm/core/serialization.h
@@ -20,10 +20,10 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
- */
-
+*/
 #ifndef OPENFDCM_SERIALIZATION_H
 #define OPENFDCM_SERIALIZATION_H
+
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -31,72 +31,37 @@ SOFTWARE.
 #include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <cstring>
+#include <ctime>
+#include <packio/core/serializable.h>
 #include "openfdcm/core/version.h"
 #include "openfdcm/core/math.h"
 
-#ifdef __GNUC__
-#define OPENFDCM_PACKED(...) __VA_ARGS__ __attribute__((__packed__))
-#elif _MSC_VER
-#define OPENFDCM_PACKED(...) __pragma( pack(push, 1) ) __VA_ARGS__ __pragma( pack(pop))
-#else
-    #error "Unsupported compiler"
-#endif
+namespace openfdcm::core {
 
-namespace openfdcm::core
-{
+    PACKIO_PACKED(struct LinesSerialHeader {
+        uint16_t fileSourceID = 0;
+        uint32_t guidData1 = 0;
+        uint16_t guidData2 = 0;
+        uint16_t guidData3 = 0;
+        std::array<unsigned char, 8> guidData4 = {0};
+        uint16_t versionMajor{}, versionMinor{}, versionPatch{};
+        uint16_t creationDay{}, creationYear{};
+        uint16_t headerSize{};
+        uint32_t offsetToLineData{};
+        unsigned char lineDataFormat{};
+        uint16_t lineDataRecordLen{};
+        uint64_t lineRecordNum{};
 
-OPENFDCM_PACKED(struct LinesSerialHeader {
-    char fileSignature[5]{};
-    uint16_t fileSourceID = 0;
-    uint32_t guidData1 = 0;
-    uint16_t guidData2 = 0;
-    uint16_t guidData3 = 0;
-    unsigned char guidData4[8] = {0};
-    uint16_t versionMajor{}, versionMinor{}, versionPatch{};
-    uint16_t creationDay{}, creationYear{};
-    uint16_t headerSize{};
-    uint32_t offsetToLineData{};
-    unsigned char lineDataFormat{};
-    uint16_t lineDataRecordLen{};
-    uint64_t lineRecordNum{};
+        [[nodiscard]] uint32_t GetLineRecordsCount() const { return lineRecordNum; }
+    });
 
-    [[nodiscard]] uint32_t GetLineRecordsCount() const { return lineRecordNum; }
-});
-
-    template <class InputCharIterator>
-    inline void readBytes(InputCharIterator &it, InputCharIterator &end, char* _ptr, size_t _size){
-        for(int i{0}; i< int(_size); i++){
-            if (it == end)
-                throw std::runtime_error{"Unexpected end of stream."};
-            char* it_ptr = _ptr+i;
-            *it_ptr = *it;
-            it++;
-        }
-    };
-
-    template <class OutputCharIterator>
-    inline void writeBytes(OutputCharIterator &it, char* _ptr, size_t _size){
-        for(int i{0}; i< int(_size); i++){
-            char* it_ptr = _ptr+i;
-            *it = *it_ptr;
-            ++it;
-        }
-    };
-
-    template <class OutputCharIterator>
-    void serializeLines(LineArray const& linearray, OutputCharIterator &&it) {
+    inline void serializeLines(LineArray const& linearray, std::ostream &stream) {
         time_t current_time = time(nullptr);
-        // convert now to tm struct for UTC
         tm *gmtm = gmtime(&current_time);
 
         // Make Header
         LinesSerialHeader header{};
-        std::memcpy(header.fileSignature, "FDCML", 5);
-        header.fileSourceID = 0;
-        header.guidData1 = 0;
-        header.guidData2 = 0;
-        header.guidData3 = 0;
         header.versionMajor = (uint16_t)OPENFDCM_VER_MAJOR;
         header.versionMinor = (uint16_t)OPENFDCM_VER_MINOR;
         header.versionPatch = (uint16_t)OPENFDCM_VER_PATCH;
@@ -108,69 +73,80 @@ OPENFDCM_PACKED(struct LinesSerialHeader {
         header.lineDataRecordLen = sizeof(LineArray::Scalar) * LineArray::RowsAtCompileTime;
         header.lineRecordNum = linearray.cols();
 
-        writeBytes(it, (char*)&header, sizeof(LinesSerialHeader));
-        writeBytes(it, (char*)linearray.data(), linearray.size() * header.lineRecordNum);
+        // Write Header
+        stream.write(reinterpret_cast<const char*>(&header), sizeof(LinesSerialHeader));
+        // Write Line Data
+        stream.write(reinterpret_cast<const char*>(linearray.data()), linearray.size() * sizeof(LineArray::Scalar));
     }
 
-    template <class InputCharIterator>
-    LineArray deserializeLines(InputCharIterator &&it, InputCharIterator &&end) {
+    inline LineArray deserializeLines(std::istream &stream) {
         LinesSerialHeader header;
 
         // Read Header
-        readBytes(it, end, reinterpret_cast<char *>(&header), sizeof(LinesSerialHeader));
+        stream.read(reinterpret_cast<char*>(&header), sizeof(LinesSerialHeader));
 
-        if (std::strncmp(header.fileSignature, "FDCML", 5) != 0) {
+        if (header.lineDataFormat != 0) {
             throw std::runtime_error(
-                    std::string("FDCM parse error: wrong magic header, found <") + header.fileSignature +
-                    std::string(">"));
+                    "Line data format not recognized, found <" + std::to_string(header.lineDataRecordLen) + ">");
         }
 
+        // Read Line Data
         LineArray linearray(4, header.lineRecordNum);
-        if (header.lineDataFormat != 0)
-            throw std::runtime_error(
-                    std::string("Line data format not recognised, found <") +
-                    std::to_string(header.lineDataRecordLen) + std::string(">"));
-
-        // Read GenericLinessizeof(GenericLine)
-        readBytes(it, end, (char*)linearray.data(), header.lineRecordNum * header.lineDataRecordLen);
+        stream.read(reinterpret_cast<char*>(linearray.data()), header.lineRecordNum * header.lineDataRecordLen);
         return linearray;
     }
 
-    template LineArray deserializeLines<>(std::istreambuf_iterator<char>&, std::istreambuf_iterator<char>&);
-
-    bool doesFileExists(const std::string& _path){
-        // From https://stackoverflow.com/a/12774387/10631984
+    bool doesFileExist(const std::string& _path) {
         struct stat buffer{};
-        return (stat (_path.c_str(), &buffer) == 0);
-    };
+        return (stat(_path.c_str(), &buffer) == 0);
+    }
 
-    void write(const std::string& _filepath, LineArray const& linearray){
-        if (doesFileExists(_filepath)){
-            if( std::remove( _filepath.c_str() ) != 0 )
-                throw std::runtime_error("File '" + _filepath + "' cant be overwritten");
+    inline void write(const std::string& _filepath, LineArray const& linearray) {
+        if (doesFileExist(_filepath)) {
+            if (std::remove(_filepath.c_str()) != 0) {
+                throw std::runtime_error("File '" + _filepath + "' can't be overwritten");
+            }
         }
 
-        std::ofstream ofs(_filepath, std::ios::out | std::ofstream::binary );
-        if (!ofs.good())
-            throw std::runtime_error(std::string("Cannot write file ") + _filepath);
-
-        serializeLines(linearray, std::ostreambuf_iterator<char>(ofs));
+        std::ofstream ofs(_filepath, std::ios::out | std::ios::binary);
+        if (!ofs.good()) {
+            throw std::runtime_error("Cannot write file '" + _filepath + "'");
+        }
+        packio::serialize(linearray, ofs);
         ofs.close();
     }
 
-    LineArray read(const std::string& _filepath){
-        if (!doesFileExists(_filepath)){
-            throw std::runtime_error("File '" + _filepath + "' does not exists");
+    inline LineArray read(const std::string& _filepath) {
+        if (!doesFileExist(_filepath)) {
+            throw std::runtime_error("File '" + _filepath + "' does not exist");
         }
 
-        std::ifstream ifs(_filepath, std::ios::in | std::ofstream::binary );
-        if (!ifs.good())
-            throw std::runtime_error(std::string("Cannot open file ") + _filepath);
-        const LineArray& lines = deserializeLines(std::istreambuf_iterator<char>(ifs),
-                std::istreambuf_iterator<char>());
+        std::ifstream ifs(_filepath, std::ios::in | std::ios::binary);
+        if (!ifs.good()) {
+            throw std::runtime_error("Cannot open file '" + _filepath + "'");
+        }
+
+        const LineArray& lines = packio::deserialize<LineArray>(ifs);
         ifs.close();
         return lines;
     }
+} // namespace openfdcm::core
 
-} //namespace openfdcm
-#endif //OPENFDCM_SERIALIZATION_H
+namespace packio {
+    template<>
+    constexpr std::array<char, 16> serializeSignature<openfdcm::core::LineArray>() {
+        return {'O', 'P', 'E', 'N', 'F', 'D', 'C', 'M',};
+    }
+
+    template<>
+    void serializeBody(const openfdcm::core::LineArray &serializable, std::ostream &stream) {
+        return openfdcm::core::serializeLines(serializable, stream);
+    }
+
+    template<>
+    openfdcm::core::LineArray deserializeBody<openfdcm::core::LineArray>(std::istream &stream) {
+        return openfdcm::core::deserializeLines(stream);
+    }
+}
+
+#endif // OPENFDCM_SERIALIZATION_H
